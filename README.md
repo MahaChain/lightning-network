cae64e47a20494965b0fefc8897f43fcbcf66364
Steven Enamakel <enamakel@mahadao.com>
2023-01-20 21:58:48 +0530

    added oayment channel contract

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
-# Sample Hardhat Project
+# MahaChain Lightning Network
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
-```
+A lightning network prototype for the MahaChain blockchain facilitating transactions of upto 1,000,000 TPS.
---
+++ contracts/PaymentChannel.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.9;
+
+import {VersionedInitializable} from './proxy/VersionedInitializable.sol';
+
+contract PaymentChannel is VersionedInitializable {
+    struct PaymentChannelData {
+        address owner;
+        uint256 value;
+        uint256 validUntil;
+        bool valid;
+    }
+
+    mapping(bytes32 => PaymentChannelData) public channels;
+    uint256 public lastId;
+
+    event NewChannel(address indexed owner, bytes32 channel);
+    event Deposit(address indexed owner, bytes32 indexed channel);
+    event Claim(address indexed who, bytes32 indexed channel);
+    event Reclaim(bytes32 indexed channel);
+
+    function initialize() external initializer {
+        lastId = 0;
+    }
+
+    function getRevision() public pure virtual override returns (uint256) {
+        return 0;
+    }
+
+    function createChannel() external payable {
+        bytes32 channel = keccak256(abi.encode(lastId++));
+        channels[channel] = PaymentChannelData(
+            msg.sender,
+            msg.value,
+            block.timestamp + 1 days,
+            true
+        );
+
+        emit NewChannel(msg.sender, channel);
+    }
+
+    // creates a hash using the recipient and value.
+    function getHash(
+        bytes32 channel,
+        address recipient,
+        uint value
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encode(channel, recipient, value));
+    }
+
+    // verify a message (receipient || value) with the provided signature
+    function verify(
+        bytes32 channel,
+        address recipient,
+        uint value,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public view returns (bool) {
+        PaymentChannelData memory ch = channels[channel];
+        return
+            ch.valid &&
+            ch.validUntil > block.timestamp &&
+            ch.owner == ecrecover(getHash(channel, recipient, value), v, r, s);
+    }
+
+    // claim funds
+    function claim(
+        bytes32 channel,
+        address recipient,
+        uint value,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        if (!verify(channel, recipient, value, v, r, s)) return;
+
+        PaymentChannelData memory ch = channels[channel];
+        if (value > ch.value) {
+            payable(recipient).transfer(ch.value);
+            ch.value = 0;
+        } else {
+            payable(recipient).transfer(value);
+            ch.value -= value;
+        }
+
+        // channel is no longer valid
+        channels[channel].valid = false;
+
+        emit Claim(recipient, channel);
+    }
+
+    function deposit(bytes32 channel) public payable {
+        require(isValidChannel(channel));
+
+        PaymentChannelData memory ch = channels[channel];
+        ch.value += msg.value;
+
+        emit Deposit(msg.sender, channel);
+    }
+
+    // reclaim a channel
+    function reclaim(bytes32 channel) public {
+        PaymentChannelData memory ch = channels[channel];
+        if (ch.value > 0 && ch.validUntil < block.timestamp) {
+            payable(ch.owner).transfer(ch.value);
+            delete channels[channel];
+        }
+    }
+
+    function getChannelValue(bytes32 channel) public view returns (uint256) {
+        return channels[channel].value;
+    }
+
+    function getChannelOwner(bytes32 channel) public view returns (address) {
+        return channels[channel].owner;
+    }
+
+    function getChannelValidUntil(bytes32 channel) public view returns (uint) {
+        return channels[channel].validUntil;
+    }
+
+    function isValidChannel(bytes32 channel) public view returns (bool) {
+        PaymentChannelData memory ch = channels[channel];
+        return ch.valid && ch.validUntil >= block.timestamp;
+    }
+}
